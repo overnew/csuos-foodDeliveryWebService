63ff95737da2f74de6b50f77e0fca5f46ee690cc
overnew <ehdls1303@gmail.com>
2022-11-04 17:39:00 +0900

    init project

---
+++ src/main/java/uoscs/rescue/foodDeliveryWebService/FoodDeliveryWebServiceApplication.java
@@ -0,0 +1,13 @@
+package uoscs.rescue.foodDeliveryWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryWebServiceApplication.class, args);
+	}
+
+}
