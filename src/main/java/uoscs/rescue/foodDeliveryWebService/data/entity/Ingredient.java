d339eed9722ef4c03a6f556314a74d75a62b488a
overnew <ehdls1303@gmail.com>
2022-11-14 16:17:36 +0900

    Feat: Refer with Stock & Ingredient
    - Stock column field join with Ingredient

---
+++ src/main/java/uoscs/rescue/foodDeliveryWebService/data/entity/Ingredient.java
@@ -0,0 +1,35 @@
+package uoscs.rescue.foodDeliveryWebService.data.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Builder
+@Getter
+@Setter
+@Entity(name = "ingredient")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Ingredient {
+    @Id
+    @Column(name = "ingredient_id")
+    private String id;
+
+    private String name;
+    private int quantity;
+
+    public void addQuantity(int quantity){
+        this.quantity += quantity;
+    }
+
+    public boolean consumeQuantity(int consumeQuantity){
+        if(this.quantity - consumeQuantity <0)
+            return false;
+
+        this.quantity -= consumeQuantity;
+        return true;
+    }
+}
--- src/main/java/uoscs/rescue/foodDeliveryWebService/data/entity/Stock.java
+++ src/main/java/uoscs/rescue/foodDeliveryWebService/data/entity/Stock.java
@@ -2,12 +2,10 @@ package uoscs.rescue.foodDeliveryWebService.data.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Builder
+@Setter
 @Getter
 @Entity(name = "stock")
 @AllArgsConstructor
@@ -17,12 +15,35 @@ public class Stock {
     @Id
     private String id;
 
-    private int steak;
-    private int bacon;
-    private int eggScramble;
-    private int bread;
-    private int salad;
-    private int coffee;
-    private int wine;
-    private int champagne;
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient steak;
+
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient bacon;
+
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient eggScramble;
+
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient bread;
+
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient salad;
+
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient coffee;
+
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient wine;
+
+    @OneToOne
+    @JoinColumn//(name = "ingredient_id")
+    private Ingredient champagne;
 }
