77dd809eff08863e4ddc82f608bf3b6792181ce4
overnew <ehdls1303@gmail.com>
2022-11-18 12:22:38 +0900

    Feat: Check Id duplicate when sign up
    - response with Response Form

--- src/main/java/uoscs/rescue/foodDeliveryWebService/controller/SignController.java
+++ src/main/java/uoscs/rescue/foodDeliveryWebService/controller/SignController.java
@@ -1,5 +1,8 @@
 package uoscs.rescue.foodDeliveryWebService.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,8 @@ import uoscs.rescue.foodDeliveryWebService.utils.SessionConst;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,13 +25,22 @@ public class SignController {
 
     private final MemberService memberService;
 
+    @ApiOperation(
+            value = "로그인"
+            ,notes = "paramter에 id와 password 전송"
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message ="로그인 성공"),
+            @ApiResponse(code= 400, message = "로그인 실패")
+    })
     @PostMapping("/signin")
-    public ResponseEntity<String> signin(HttpServletRequest request){
+    public ResponseEntity<ResponseForm> signin(HttpServletRequest request){
         String id = request.getParameter("id");
         String password = request.getParameter("password");
 
-        if(!memberService.checkIdAndPassword(id,password))
-            return ResponseEntity.ok("fail");
+        if(!memberService.checkIdAndPassword(id,password)){
+            return ResponseEntity.badRequest().body(buildResponseForm(false,"아이디나 비밀번호가 올바르지 않습니다."));
+        }
 
         //id, password 통과, 권한 찾아오기
         MemberDto memberDto = memberService.findById(id);
@@ -35,16 +49,38 @@ public class SignController {
         HttpSession session = request.getSession(); //새로 만드는 거니까 true
         session.setAttribute(SessionConst.SESSION_FORM, sessionForm);
 
-        return ResponseEntity.ok("login success id= " +id);
+        return ResponseEntity.ok(buildResponseForm(true,"로그인 성공"));
     }
 
+
+    @ApiOperation(
+            value = "회원 가입"
+            ,notes = "MemberDto에 데이터 채워서 요청"
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message ="회원가입 성공"),
+            @ApiResponse(code= 400, message = "회원가입 실패")
+    })
     @PostMapping("/signup")
     public ResponseEntity<ResponseForm> signup(@Valid @RequestBody MemberDto memberDto){
-        memberService.save(memberDto);
+        MemberDto savedMemberDto = memberService.saveWithCheckId(memberDto);
 
-        ResponseForm responseForm = ResponseForm.builder().success(true).build();
+        if(savedMemberDto ==null){
+            return ResponseEntity.badRequest().body(buildResponseForm(false,"중복된 아이디가 존재합니다."));
+        }
 
-        return ResponseEntity.ok(responseForm);
+        return ResponseEntity.ok(buildResponseForm(true,"회원가입 성공"));
     }
+
+    private ResponseForm buildResponseForm(Boolean isSuccess,String message){
+        List<String> messages = new ArrayList<>();
+        messages.add(message);
+
+        return ResponseForm.builder()
+                .success(isSuccess)
+                .messages(messages)
+                .build();
+    }
+
 
 }
--- src/main/java/uoscs/rescue/foodDeliveryWebService/service/MemberService.java
+++ src/main/java/uoscs/rescue/foodDeliveryWebService/service/MemberService.java
@@ -5,8 +5,10 @@ import uoscs.rescue.foodDeliveryWebService.data.entity.Member;
 
 public interface MemberService {
     MemberDto save(MemberDto memberDto);
+    MemberDto saveWithCheckId(MemberDto memberDto);
 
     boolean checkIdAndPassword(String id, String password);
+
     MemberDto findById(String id);
 
 }
