e3233f51a8940e4280312701fbc59be1e9940187
seongho9 <91736237+seongho9@users.noreply.github.com>
2022-11-27 17:36:43 +0900

    feat: gogo test

--- src/main/front/src/components/PostStock.js
+++ src/main/front/src/components/PostStock.js
@@ -3,20 +3,7 @@ import { StockContext } from "../View/StockManageView";
 
 const PostStock = () => {
     const stock = useContext(StockContext);
-    const onClick = async () => {
-        await fetch("/manage/stock", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                stock
-            },
-            body: JSON.stringify(stock)
-        }).then((res) => {
-            return res.json();
-        }).then((res) => {
-            console.log(res);
-        })
-    }
+    
     return (
         <div>
             <button type='button' onClick={onClick}>적용</button>
