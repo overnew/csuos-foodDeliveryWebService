dd168c4010bcbbc9b50480020f76f737892d5b40
seongho9 <91736237+seongho9@users.noreply.github.com>
2022-11-28 14:22:34 +0900

    fix:poststock

--- src/main/front/src/components/PostStock.js
+++ src/main/front/src/components/PostStock.js
@@ -3,7 +3,21 @@ import { StockContext } from "../View/StockManageView";
 
 const PostStock = () => {
     const stock = useContext(StockContext);
-    
+    const onClick = async() => {
+        await fetch('/manage/stock', {
+            method: 'POST',
+            credentials:'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                stock
+            },
+            body: JSON.stringify(stock)
+        }).then((res) => {
+            return res.json();
+        }).then((json) => {
+            console.log(json);
+        })
+    }
     return (
         <div>
             <button type='button' onClick={onClick}>적용</button>
